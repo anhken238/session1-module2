201ee598f8bbb0aa149aca075316da80515b8e6c
Nguyen Duc Anh <anhken238@gmail.com>
2019-06-03 17:29:45 +0700

    chi so can nang

--- uocsochungMax.java
+++ uocsochungMax.java
@@ -1,4 +1,26 @@
 package session1;
-
+import java.util.Scanner;
 public class uocsochungMax {
+    public static void main(String[] args) {
+
+     Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập a: ");
+        int a = scanner.nextInt();
+        System.out.println(("Nhập b: "));
+        int b = scanner.nextInt();
+
+        Math.abs(a);
+        Math.abs(b);
+
+        if(a == 0 || b == 0){
+            System.out.println("Không có ước chung lớn nhất");
+        }
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println("ước số chung lớn nhất là : " + a);
+    }
 }
